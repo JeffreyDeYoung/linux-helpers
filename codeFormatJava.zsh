989d3db8ec274ba72acb7cf9cad4db9e192d5f36
Jeffrey DeYoung <jeffrey.deyoung@pearson.com>
2016-08-04 18:05:56 -0600

    fixed file extension for format script

---
+++ codeFormatJava.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+#author: Jeffrey DeYoung
+#Use to code format java files per the google style guide.
+#Eclipse is required for this script to work; it's basically wrapping an Eclipse command.
+#Requires zsh because I don't want to write an if statment for bash.
+#
+#To use, add to your shells .zshrc or .bashrc file.
+#Ex: 
+#ECLIPSE_BIN=~/eclipse/java-neon/eclipse/eclipse
+#source ~/projects/linuxscripts/codeFormatJava.zsh
+
+
+function codeFormatJava()
+{
+	called=$_    
+	echo "Performing Java Code Format per Google Style Guide on current directory: `pwd`..."	
+	#echo $called
+	DIR=$(dirname ${(%):-%x})
+	#echo $DIR
+    $ECLIPSE_BIN -nosplash -application org.eclipse.jdt.core.JavaCodeFormatter -config /home/jeffrey/workspace/test/.settings/org.eclipse.jdt.core.prefs .
+}
+
+#handy alias for calling this
+alias javaformat="codeFormatJava"
+
